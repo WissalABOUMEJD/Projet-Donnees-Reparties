08c15a5234d70f6764bd9682d4e90027785a5b5d
AkkarKhadija24 <khadijakkarah@gmail.com>
2023-12-12 09:19:21 +0100

    my commit

---
+++ hagidoop/src/io/ImplNetworkReaderWriter.java
@@ -0,0 +1,50 @@
+package io;
+
+import interfaces.KV;
+import interfaces.NetworkReaderWriter;
+
+public class ImplNetworkReaderWriter implements NetworkReaderWriter{
+
+	@Override
+	public KV read() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void write(KV record) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void openServer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void openClient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public NetworkReaderWriter accept() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void closeServer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeClient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
