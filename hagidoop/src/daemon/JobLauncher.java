b19c994abd6f7b4de00665f92dad8948f57373e5
Wissal Aboumejd <89734216+WissalABOUMEJD@users.noreply.github.com>
2024-09-28 22:10:12 +0200

    Update JobLauncher.java

--- hagidoop/src/daemon/JobLauncher.java
+++ hagidoop/src/daemon/JobLauncher.java
@@ -1,39 +1,59 @@
 package daemon;
-import hdfs.HdfsClient;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
-
-import interfaces.FileReaderWriter;
-import interfaces.MapReduce;
-import interfaces.NetworkReaderWriter;
 import io.KVFileRW;
 import io.TxtFileRW;
-import interfaces.KV;
+import config.Project;
+import interfaces.MapReduce;
 
 public class JobLauncher {
 	static int nbMachines=2;
-	//static Worker[] listeWorkers = new Worker[nbMachines];
-	//String[] urlWorker = {"//localhost:8000/test","//localhost:8001/test", "//localhost:8002/test", "//localhost:8003/test", "//localhost:8004/test"};
+	static Worker[] listeWorkers = new Worker[nbMachines];
+	static String[] urlWorker = {"//localhost:8000/test","//localhost:8001/test", "//localhost:8002/test", "//localhost:8003/test", "//localhost:8004/test"};
 	
 	public static void startJob (MapReduce mr, int format, String fname) {
-//		int nbfragments = 3;
-//		for (int i = 0 ; i < nbfragments; i++) {
-//			TxtFileRW reader = new TxtFileRW(fname+"filesample-"+i+".txt");
-//			KVFileRW writer = new KVFileRW(fname+"filesample-"+i+"res.txt");
-//			System.out.println("****************1111");
-//			try {
-//				listeWorkers[i%nbMachines].runMap(mr, reader, writer);
-//				System.out.println("****************");
-//				writer.open("read");
-//				KV kv;
-//	            while ((kv = writer.read()) != null) {
-//	                System.out.println("Key: " + kv.k + ", Value: " + kv.v);
-//	            }
-//	            System.out.println("khadiiiiiiiiija");
-//			} catch (RemoteException e) {
-//				e.printStackTrace();
-//			}
-//		}
-		
+
+		hdfs.HdfsClient.HdfsWrite(0, fname);
+		try{
+			for (int i = 0 ; i < nbMachines ; i++) {
+				listeWorkers[i]=(Worker) Naming.lookup(urlWorker[i]);
+			}
+			//MyMapReduce mr = new MyMapReduce();
+			for (int i = 0 ; i < 7; i++) {  //nbFRAGMEEEEENTS;
+				try {
+					TxtFileRW reader = new TxtFileRW(listeWorkers[i%2].getPath()+"//data//filesample-"+i+".txt");
+					KVFileRW writer = new KVFileRW(listeWorkers[i%2].getPath()+"//data//filesampleres-"+i+".txt");
+					listeWorkers[i%nbMachines].runMap(mr, reader, writer);
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+			}
+			//java.lang.Thread.sleep(30);
+//			JobLauncher job = new JobLauncher();
+//			System.out.println("Lancement du Job");
+//			job.startJob(mr, 0, "\\home\\wissal\\Bureau\\pdr\\Projet-Donnees-Reparties\\hagidoop\\src\\");
+//			System.out.println("Fin du lancement du Job");
+			//System.out.println("Lecture COMMENCÉÉÉÉ");
+			hdfs.HdfsClient.HdfsRead(Project.PATH+"//data//filesampleFinal.txt");
+			//System.out.println("Lecture terminée");
+
+			KVFileRW reader = new KVFileRW(Project.PATH+"//data//filesampleFinal.txt");
+			KVFileRW writer = new KVFileRW(Project.PATH+"//data//resultat.txt");
+			reader.open("read");
+			writer.open("write");
+			
+			// appliquer reduce sur le résultat
+			// reader : format kv ; writer : format kv
+			//System.out.println("Début du reduce");
+			
+			mr.reduce(reader, writer);
+			//System.out.println("Fin du reduce");
+			
+			reader.close();
+			writer.close();
+		} catch (Exception exc)  {
+			exc.printStackTrace();
+
+		}
 	}
 }
