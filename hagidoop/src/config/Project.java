807414e7ea301ac7cc36ae12c271392a811dfbe9
Wissal Aboumejd <89734216+WissalABOUMEJD@users.noreply.github.com>
2024-09-28 22:08:01 +0200

    Update Project.java

--- hagidoop/src/config/Project.java
+++ hagidoop/src/config/Project.java
@@ -1,10 +1,18 @@
 package config;
+import java.io.File;
 
 public class Project {
-	public static String PATH = "/home/hagimont/hagidoop/";
+	public static String PATH = new File("").getAbsolutePath();
 	public static String listMachines[] = {"localhost", "localhost", "localhost"};
 	public static int listServeur[] = {4000, 4001, 4002};
 	public enum Cmd {READ , WRITE , DELETE };
 	public static int fragmentsize = 128000;
 	public static int nbrFragments = 128000;
+	
+	public static void main(String[] args) {
+          System.out.print(PATH);
+	}
 }
+
+	
+	
